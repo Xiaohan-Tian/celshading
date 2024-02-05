8e5926a10f5e28e8e06b4978aa4cfe2d6fd2cdc1
Xiaohan Tian <xiaohan.tian@gmail.com>
2024-02-04 17:45:13 -0800

    fixed some issues in publish action

--- .github/increase_version.py
+++ .github/increase_version.py
@@ -1,18 +1,19 @@
 import re
 
-def update_version(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+file_path = "src/__init__.py"
 
-    for i, line in enumerate(lines):
-        if line.strip().startswith('"version":'):
-            version_info = re.search(r'\((\d+), (\d+), (\d+)\)', line)
-            if version_info:
-                major, minor, subminor = map(int, version_info.groups())
-                subminor += 1  # Increment the subminor version
-                new_version = f'    "version": ({major}, {minor}, {subminor}),\n'
-                lines[i] = new_version
-                break
+with open(file_path, 'r') as file:
+    lines = file.readlines()
 
-    with open(file_path, 'w') as file:
-        file.writelines(lines)
+for i, line in enumerate(lines):
+    if line.strip().startswith('"version":'):
+        version_info = re.search(r'\((\d+), (\d+), (\d+)\)', line)
+        if version_info:
+            major, minor, subminor = map(int, version_info.groups())
+            subminor += 1  # Increment the subminor version
+            new_version = f'    "version": ({major}, {minor}, {subminor}),\n'
+            lines[i] = new_version
+            break
+
+with open(file_path, 'w') as file:
+    file.writelines(lines)
